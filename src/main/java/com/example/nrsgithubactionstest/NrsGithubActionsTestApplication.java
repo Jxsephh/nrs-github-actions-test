c4d3b7ed3170863f9338788506d5cfc8bee7428e
Joseph Santana <joseph.santanamartinez@aa.com>
2022-08-12 14:45:59 -0500

    added maven project and github workflow

---
+++ src/main/java/com/example/nrsgithubactionstest/NrsGithubActionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nrsgithubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrsGithubActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrsGithubActionsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nrsgithubactionstest/NrsGithubActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nrsgithubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NrsGithubActionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
