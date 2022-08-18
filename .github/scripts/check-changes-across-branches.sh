fd1f6807439848c0a16cfda5c8a41ede3b753e95
Joseph Santana <joseph.santanamartinez@aa.com>
2022-08-18 09:27:45 -0500

    Update soaplib version if changes exist

--- .github/scripts/check-changes-across-branches.sh
+++ .github/scripts/check-changes-across-branches.sh
@@ -1,7 +1,7 @@
 # =============================================================================
 #
 # This script checks to see if two git branches are different and returns 0 if
-# they are the same, and 1 if they are different
+# they are the same, and 1 if they are different.
 # The script takes in the path to the repository as argument 1, along with the
 # two branches to compare as arguments 2 and 3.
 # 
